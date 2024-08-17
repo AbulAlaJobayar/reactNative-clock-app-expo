a23ba437f2664904a7abb5c3eab6ccc7cc63cf0d
AbulAlaJobayar <jobayar59@gmail.com>
2024-08-17 19:59:59 +0600

    top part complate

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    inter_Regular: Inter_400Regular,
+    inter_Bold: Inter_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/light-bg.png")}
+      style={{ flex: 1 }}
+    >
+      {/* container */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          marginTop: 32,
+          paddingHorizontal: 12,
+        }}
+      >
+        {/* top Part */}
+        <View style={{ flexDirection: "row" }}>
+          <View>
+            <Text
+              style={{
+                fontFamily: "inter_Regular",
+                fontSize: 12,
+                color: "#FFFFFF",
+              }}
+            >
+              “The science of operations, as derived from mathematics more
+              especially, is a science of itself, and has its own abstract truth
+              and value.”
+            </Text>
+            <Text
+              style={{
+                fontFamily: "inter_Bold",
+                color: "#FFFFFF",
+                fontSize: 12,
+                marginTop: 8,
+              }}
+            >
+              - Ada Lovelace
+            </Text>
+          </View>
+          <Image source={require("./assets/refresh.png")} />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

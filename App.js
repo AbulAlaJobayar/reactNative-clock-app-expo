709216af2b31c18ae5334f8046cc76c746dcc295
AbulAlaJobayar <jobayar59@gmail.com>
2024-08-17 23:36:20 +0600

    add roleview

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import {
@@ -13,6 +14,44 @@ import {
   Inter_400Regular,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
+
+const RoleView = ({ label, view }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 8,
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-Regular",
+            color: "#303030",
+            fontSize: 10,
+            letterSpacing: 2,
+            textTransform: "uppercase",
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-Bold",
+            fontSize: 20,
+            color: "#303030",
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -36,7 +75,7 @@ export default function App() {
           paddingHorizontal: 12,
         }}
       >
-        {/* top Part */}
+        {/* top portion*/}
         <View style={{ flexDirection: "row" }}>
           <View>
             <Text
@@ -63,6 +102,100 @@ export default function App() {
           </View>
           <Image source={require("./assets/refresh.png")} />
         </View>
+        {/* BOTTOM PORTION */}
+
+        <View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image source={require("./assets/sun.png")} />
+            <Text
+              style={{
+                fontFamily: "Inter_Regular",
+                fontSize: 15,
+                color: "#FFFFFF",
+                marginLeft: 8,
+                letterSpacing: 3,
+              }}
+            >
+              GOOD MORNING
+            </Text>
+          </View>
+          <View>
+            <Text>
+              <Text
+                style={{
+                  fontFamily: "Inter_Bold",
+                  fontSize: 100,
+                  color: "#FFFFFF",
+                }}
+              >
+                11:37
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "inter_Regular",
+                  fontSize: 15,
+                  color: "#FFFFFF",
+                }}
+              >
+                BST
+              </Text>
+            </Text>
+          </View>
+          {/* Location */}
+          <View style={{ marginTop: 16 }}>
+            <Text
+              style={{
+                fontFamily: "Inter_Bold",
+                fontSize: 15,
+                color: "#FFFFFF",
+                letterSpacing: 3,
+              }}
+            >
+              IN LONDON, UK
+            </Text>
+          </View>
+
+          {/* BUTTON */}
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              flexDirection: "row",
+              height: 40,
+              width: 115,
+              backgroundColor: "#fff",
+              borderRadius: 30,
+              marginTop: 50,
+              justifyContent: "space-between",
+              paddingLeft: 16,
+              paddingRight: 4,
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "inter_Bold",
+                fontSize: 12,
+                color: "#000",
+                letterSpacing: 3,
+              }}
+            >
+              MORE
+            </Text>
+            <Image source={require("./assets/arrow-down.png")} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* EXPANDED View */}
+      <View
+        style={{
+          backgroundColor: "#fff",
+          opacity: 0.8,
+          paddingVertical: 48,
+          paddingHorizontal: 26,
+        }}
+      >
+
+        <RoleView label={"Current Timezone"} value="Europe/London"/>
       </View>
     </ImageBackground>
   );
